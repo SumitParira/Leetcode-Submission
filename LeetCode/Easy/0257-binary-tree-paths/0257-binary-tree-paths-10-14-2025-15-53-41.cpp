2d112178f9b47b1ff1f9b8d48b9e7f51db2dae6b
SumitParira <155645426+SumitParira@users.noreply.github.com>
2025-10-14 15:53:42 +0530

    Time: 0 ms (100%), Space: 17.9 MB (33.88%) - LeetHub

---
+++ LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths-10-14-2025-15-53-41.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+vector<string> ans;
+
+void solve(TreeNode* root,string s){
+    if(root==NULL) return ;
+
+    s+= to_string(root->val);
+
+    if(root->left==NULL && root->right==NULL) {
+        ans.push_back(s);
+        return;
+    }
+
+    s+="->";
+
+    solve(root->left,s);
+    solve(root->right,s);
+
+
+}
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string s = "";
+        solve(root,s);
+        return ans;
+    }
+};

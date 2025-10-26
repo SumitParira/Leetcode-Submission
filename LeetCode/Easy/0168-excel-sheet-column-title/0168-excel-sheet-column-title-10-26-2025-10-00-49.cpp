012f51f2b2888d4475b023a446f9f2ae87438123
SumitParira <155645426+SumitParira@users.noreply.github.com>
2025-10-26 10:00:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (1.47%) - LeetHub

---
+++ LeetCode/Easy/0168-excel-sheet-column-title/0168-excel-sheet-column-title-10-26-2025-10-00-49.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+ unordered_map<int, char> mp = {
+    {0, 'A'}, {1, 'B'}, {2, 'C'}, {3, 'D'}, {4, 'E'},
+    {5, 'F'}, {6, 'G'}, {7, 'H'}, {8, 'I'}, {9, 'J'},
+    {10, 'K'}, {11, 'L'}, {12, 'M'}, {13, 'N'}, {14, 'O'},
+    {15, 'P'}, {16, 'Q'}, {17, 'R'}, {18, 'S'}, {19, 'T'},
+    {20, 'U'}, {21, 'V'}, {22, 'W'}, {23, 'X'}, {24, 'Y'},
+    {25, 'Z'}
+};
+
+
+
+string s = "";
+while(columnNumber>0){
+    columnNumber--;
+    s+=mp[columnNumber%26];
+    columnNumber/=26;
+    
+}
+reverse(s.begin(),s.end());
+
+return s;
+        
+    }
+};

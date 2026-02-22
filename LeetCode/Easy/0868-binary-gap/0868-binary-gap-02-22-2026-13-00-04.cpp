5e15ea61a4e200ce7991285174f5915e2720bd97
SumitParira <155645426+SumitParira@users.noreply.github.com>
2026-02-22 13:00:04 +0530

    Time: 0 ms (100%), Space: 8 MB (70.75%) - LeetHub

---
+++ LeetCode/Easy/0868-binary-gap/0868-binary-gap-02-22-2026-13-00-04.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int ans=0;
+        int last=-1;
+        for(int i=0;i<32;i++){
+             if ((n >> i) & 1) {
+                if (last != -1) {
+                    ans = max(ans, i - last);
+                }
+                last = i;
+            }
+        }
+        return ans;
+    }
+};

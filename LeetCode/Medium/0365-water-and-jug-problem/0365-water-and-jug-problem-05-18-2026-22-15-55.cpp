7d0a45c160558bd86376fa8921a39e47c5207ea4
SumitParira <155645426+SumitParira@users.noreply.github.com>
2026-05-18 22:15:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.49%) - LeetHub

---
+++ LeetCode/Medium/0365-water-and-jug-problem/0365-water-and-jug-problem-05-18-2026-22-15-55.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int target) {
+        if(x+y<target) return false;
+        if (target == 0)    return true;
+        return target % __gcd(x, y) == 0;
+    }
+};
+
